8e79f0dfc6517eb92245d247de275a93104a28a6
Chi <chulinguy@gmail.com>
2017-09-03 14:31:14 -0700

    fixing bounce bugs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,6 +13,15 @@ $('.dropdown').on('hide.bs.dropdown', function(e){
 
 $('[data-toggle="tooltip"]').tooltip(); 
 
+ $('.icon').hover( function() {
+      $(this).attr('bounce', true);
+      bounce($(this));
+      }, function() { 
+        $(this).attr('bounce', false);
+        $(this).stop();
+      }
+    )
+
 function bounce($elem) {
     $elem.effect('bounce', { times: 1, distance: 10 }, 1200, ()=>{
       if ($elem.attr('bounce') =='true') bounce($elem);
--- assets/javascript/skype.js
+++ assets/javascript/skype.js
@@ -1,11 +1,23 @@
-setTimeout(()=>{
-  $("#SkypeButton_Call_chulinguy_1").find('img').addClass('icon')
-  $('.icon').hover( function() {
-    $(this).attr('bounce', true);
-    bounce($(this));
-    }, function() { 
-      $(this).attr('bounce', false);
-      $(this).stop();
-    }
-  )
-}, 500);
+  var counter = 0;  
+  var skype_bounce = setInterval(function(){
+    $("#SkypeButton_Call_chulinguy_1").find('img').addClass('skype-icon');
+    counter = $('.skype-icon').length;
+    $('.skype-icon').hover( function() {
+      $(this).attr('bounce', true);
+      bounce($(this));
+      }, function() { 
+        $(this).attr('bounce', false);
+        $(this).stop();
+      }
+    )
+    if (counter) clearInterval(skype_bounce);
+  }, 500);
+
+  function bounce($elem) {
+    $elem.effect('bounce', { times: 1, distance: 10 }, 1200, ()=>{
+      if ($elem.attr('bounce') =='true') bounce($elem);
+      else {
+        $elem.stop()
+      }
+    });
+}
