c716ea0f94e36738089f168a09dddf7bfd863f51
Chi <chulinguy@gmail.com>
2017-09-08 11:56:19 -0700

    portfolio page is so pretty

--- assets/javascript/portfolio-popup.js
+++ assets/javascript/portfolio-popup.js
@@ -3,14 +3,14 @@ const descObj = {};
 descObj.projJM  = ['coming soon ...'];
 descObj.projMotivate = ['This MERN app keeps track of your one goal (with calendar reminders) and rewards you for task completion with RPS elements.', 'Technologies leveraged: MongoDB, Express.js, React, React-router, Google Calendar API'];
 descObj.projRPS = ['This multi-language, two-player Rock-Paper-Scissor web game utilizes Firebase to store user\'s game choices/wins/losses, with a functional chat window', 'Technologies leveraged: JQuery, Firebase, underscore.js']
-descObj.projRM  = ['This web app creates a clear cost and time comparison between driving and taking public transport for a given trip.', 'Technologies leveraged: Google Maps API (directions/geocoding/public transit data), myGasFeed API & fueleconomy.gov API, jQuery']
+descObj.projRM  = ['This web app creates a clear cost and time comparison between driving and public transit for a given trip.', 'Technologies leveraged: Google Maps API (directions/geocoding/public transit data), myGasFeed API & fueleconomy.gov API, jQuery']
 
 
 roleObj = {};
 roleObj.projJM  = 'Sole developer';
 roleObj.projRPS = 'Sole developer';
 roleObj.projRM  = 'Lead developer in a team of four';
-roleObj.projMotivate  = 'Routing/React/passport.js specialist';
+roleObj.projMotivate  = 'Routing/React/login specialist in a team of five';
 
 $('.portfolio-container').each(function(){
   var title = $(this).find('.portfolio-h3').attr('id');
