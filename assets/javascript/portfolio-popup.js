179b420320a067fc268920f199815cd30db08a15
chulinguy <chulinguy@gmail.com>
2018-01-14 14:56:17 -0800

    fixing typos

--- assets/javascript/portfolio-popup.js
+++ assets/javascript/portfolio-popup.js
@@ -1,17 +1,17 @@
 
 const descObj = {};
-descObj.projJM  = ['This web quiz game not only tests your knowledge in Javascript and computer sciences but also contains tons of funny jokes.', 'Technologies leveraged: Chance.js, Chart.js, ResponsiveVoice.js, Firebase, jQuery'];
-descObj.projMotivate = ['This MERN app keeps track of your one goal (with calendar reminders) and rewards you for task completion with RPS elements.', 'Technologies leveraged: MongoDB, Express.js, React, React-router, Google Calendar API'];
-descObj.projRPS = ['This multi-language, two-player Rock-Paper-Scissor web game utilizes Firebase to store user\'s game choices/wins/losses, with a functional chat window', 'Technologies leveraged: JQuery, Firebase, underscore.js']
-descObj.projRM  = ['This web app creates a clear cost and time comparison between driving and public transit for a given trip.', 'Technologies leveraged: Google Maps API (directions/geocoding/public transit data), myGasFeed API & fueleconomy.gov API, jQuery']
-descObj.projTFD = ['This website shares valuable tips to beginner tango dancers', 'Technologies leveraged: React.js, SASS/Compass, image map']
+descObj.projJM  = ['This web quiz game not only tests your knowledge in Javascript and computer sciences but also contains tons of funny jokes.', 'Technologies: Chance.js, Chart.js, ResponsiveVoice.js, Firebase, jQuery'];
+descObj.projMotivate = ['This MERN app keeps track of your one goal (with calendar reminders) and rewards you for task completion with RPG elements.', 'Technologies: MongoDB, Express.js, React, React-router, Google Calendar API'];
+descObj.projRPS = ['This multi-language, two-player Rock-Paper-Scissor web game utilizes Firebase to store user\'s game choices/wins/losses, with a functional chat window', 'Technologies: JQuery, Firebase, underscore.js']
+descObj.projRM  = ['This web app creates a clear cost and time comparison between driving and public transit for a given trip.', 'Technologies: Google Maps API (directions/geocoding/public transit data), myGasFeed API & fueleconomy.gov API, jQuery']
+descObj.projTFD = ['This website shares valuable tips to beginner tango dancers', 'Technologies: React.js, SASS/Compass, image map']
 
 roleObj = {};
 roleObj.projTFD  = 'Sole developer';
 roleObj.projJM  = 'Sole developer';
 roleObj.projRPS = 'Sole developer';
 roleObj.projRM  = 'Lead developer in a team of four';
-roleObj.projMotivate  = 'Routing/React/login specialist in a team of five';
+roleObj.projMotivate  = 'React/routing/login specialist in a team of five';
 
 $('.portfolio-container').each(function(){
   var title = $(this).find('.portfolio-h3').attr('id');
