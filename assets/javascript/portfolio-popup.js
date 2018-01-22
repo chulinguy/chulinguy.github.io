fd28f318f9f40d8b0eb96c1a6490a6f8715e1413
chulinguy <chulinguy@gmail.com>
2018-01-22 13:23:48 -0800

    added github link

--- assets/javascript/portfolio-popup.js
+++ assets/javascript/portfolio-popup.js
@@ -5,7 +5,14 @@ descObj.projRPS = ['This multi-language, two-player Rock-Paper-Scissor web game 
 descObj.projRM  = ['This web app creates a clear cost and time comparison between driving and public transit for a given trip.', 'Technologies: Google Maps API (directions/geocoding/public transit data), myGasFeed API & fueleconomy.gov API, jQuery']
 descObj.projTFD = ['This website shares valuable tips to beginner tango dancers', 'Technologies: React/Redux, SASS/Compass, image map']
 
-roleObj = {};
+const githubObj = {};
+githubObj.projJM = 'https://github.com/chulinguy/JavascriptMillionaire';
+githubObj.projMotivate = 'https://github.com/briandhus/Goalie';
+githubObj.projRPS = 'https://github.com/chulinguy/RPS-multiplayer';
+githubObj.projRM = 'https://github.com/chulinguy/Trip-Comparator';
+githubObj.projTFD = 'https://github.com/chulinguy/tango-for-dummies';
+
+const roleObj = {};
 roleObj.projTFD  = 'Sole developer';
 roleObj.projJM  = 'Sole developer';
 roleObj.projRPS = 'Sole developer';
@@ -19,9 +26,11 @@ $('.portfolio-container').each(function(){
   var $panelHeadDiv = $('<div>').addClass('panel-heading text-center').text(roleObj[title]);
   var $panelBodyDiv = $('<div>').addClass('panel-body');
   descObj[title].forEach((v)=>{
-    var $pTag = $('<p>').text(v);
+    var $pTag = $('<p>').html(v);
     $panelBodyDiv.append($pTag);
   });
+  var $githubLink = $('<a>').addClass('github-link').text('Github').attr('href', githubObj[title]);
+  $panelBodyDiv.append($githubLink);
   $panelDiv.append($panelHeadDiv, $panelBodyDiv);
   $(this).append($panelDiv)
 })
